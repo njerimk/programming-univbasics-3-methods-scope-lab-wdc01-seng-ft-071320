330d824dadc1fd93178b0ec2d3e88ad645965047
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-29 19:17:03 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,26 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ 
+ def mario
+ $status
+  phrase ="It's-a me, Mario!"
   puts phrase
 end
 
+
 def toadstool
-  puts status
+  puts $status
 end
+
+def link 
+  phrase ="It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
+end
+
+all_phrases
+
